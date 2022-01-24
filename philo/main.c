51ad42584c09c5bf83fe896bb0f50a477d7205b4
majacque <majacque@student.42.fr>
2022-01-24 20:35:25 +0100

    begin to create all my philosophers [OK]

--- philo/main.c
+++ philo/main.c
@@ -6,68 +6,50 @@
 /*   By: majacque <majacque@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/21 19:42:33 by majacque          #+#    #+#             */
-/*   Updated: 2022/01/24 18:38:43 by majacque         ###   ########.fr       */
+/*   Updated: 2022/01/24 20:32:39 by majacque         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophers.h"
 
-typedef struct s_test	t_test;
-
-struct s_test
-{
-	char	*str;
-	int		nb;
-	pthread_mutex_t	*mutex;
-};
-
 static int	__error_usage(void)
 {
 	printf("usage: ./philosophers nb_of_philosophers time_to_die time_to_eat");
 	printf(" time_to_sleep [nb_of_time_each_philo_must_eat]\n");
-	return (0);
+	return (1);
 }
 
-static void	*test(void *param)
+int	philos_init(pthread_t **philos, int nb_philo)
 {
-	t_test	*data = (t_test *)param;
+	int	i;
 
-	pthread_mutex_lock(data->mutex);
-	printf("%s %d\n", data->str, data->nb);
-	pthread_mutex_unlock(data->mutex);
-	return (NULL);
+	*philos = ft_calloc(nb_philo + 1, sizeof(pthread_t));
+	if (*philos == NULL)
+		return (1);
+	i = 0;
+	while (i < nb_philo)
+	{
+		if (pthread_create(&(*philos)[i], NULL, routine, arg_routine)) // TODO routine()
+		{
+			free(*philos);
+			return (1);
+		}
+		i++;
+	}
+	return (0);
 }
 
 // ./philosophers nb_of_philo time_to_die time_to_eat time_to_sleep [nb_of_time_each_philo_must_eat]
 int	main(int argc, char **argv)
 {
 	t_inputs	inputs;
-	pthread_t	thread1;
-	pthread_t	thread2;
-	t_test		data1;
-	t_test		data2;
-	pthread_mutex_t	baton_parole;
+	pthread_t	*philos; // tableau de philos
 
 	if (argc != 5 && argc != 6)
 		return (__error_usage());
 	if (get_inputs(&inputs, argc, argv))
-		return (0);
-
-	pthread_mutex_init(&baton_parole, NULL);
-	data1.str = "Je suis le thread";
-	data1.nb = 1;
-	data1.mutex = &baton_parole;
-	data2.str = "Je suis le thread";
-	data2.nb = 2;
-	data2.mutex = &baton_parole;
-
-	pthread_create(&thread1, NULL, &test, &data1);
-	pthread_create(&thread2, NULL, &test, &data2);
-
-	pthread_join(thread1, NULL);
-	pthread_join(thread2, NULL);
-
-	pthread_mutex_destroy(&baton_parole);
-	puts("Fin");
+		return (1);
+	if (philos_init(philos, inputs.nb_philo))
+		return (1);
 	return (0);
 }
